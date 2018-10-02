a39987eec00f34ab15209f1f09bf64e98c580e89
Khai98 <hoangkhai1411111998@gmail.com>
2018-10-02 10:36:08 +0700

    new

---
+++ resources/views/index.blade.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Task Management</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+</head>
+<body>
+
+
+<div class="limiter">
+    <div class="container-table100">
+        <h1>Danh sách khách hàng</h1>
+        <div class="wrap-table100">
+            <div class="table100 ver6 m-b-110">
+                <table data-vertable="ver6">
+                    <thead>
+                    <tr class="row100 head">
+                        <th class="column100 column1" data-column="column1">STT</th>
+                        <th class="column100 column2" data-column="column2">Họ Và Tên</th>
+                        <th class="column100 column3" data-column="column3">Số Điện Thoại</th>
+                        <th class="column100 column4" data-column="column4">Email</th>
+                        <th class="column100 column5" data-column="column5">Địa Chỉ</th>
+                        <th class="column100 column6" data-column="column6">Thao Tác</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    <tr class="row100">
+                        <td class="column100 column1" data-column="column1">1</td>
+                        <td class="column100 column2" data-column="column2">Nguyễn Văn A</td>
+                        <td class="column100 column3" data-column="column3">01234567890</td>
+                        <td class="column100 column4" data-column="column4">email.test@mail.com</td>
+                        <td class="column100 column5" data-column="column5">hanoi@hanoi@hanoi</td>
+                        <td><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a></td>
+
+                    </tr>
+
+                    <tr class="row100">
+                        <td class="column100 column1" data-column="column1">2</td>
+                        <td class="column100 column2" data-column="column2">Nguyễn Văn B</td>
+                        <td class="column100 column3" data-column="column3">01234567890</td>
+                        <td class="column100 column4" data-column="column4">email.test@mail.com</td>
+                        <td class="column100 column5" data-column="column5">hanoi@hanoi@hanoi</td>
+                        <td><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a></td>
+
+                    </tr>
+
+                    <tr class="row100">
+                        <td class="column100 column1" data-column="column1">3</td>
+                        <td class="column100 column2" data-column="column2">Nguyễn Văn C</td>
+                        <td class="column100 column3" data-column="column3">01234567890</td>
+                        <td class="column100 column4" data-column="column4">email.test@mail.com</td>
+                        <td class="column100 column5" data-column="column5">hanoi@hanoi@hanoi</td>
+                        <td><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a></td>
+
+                    </tr>
+
+                    <tr class="row100">
+                        <td class="column100 column1" data-column="column1">4</td>
+                        <td class="column100 column2" data-column="column2">Nguyễn Văn D</td>
+                        <td class="column100 column3" data-column="column3">01234567890</td>
+                        <td class="column100 column4" data-column="column4">email.test@mail.com</td>
+                        <td class="column100 column5" data-column="column5">hanoi@hanoi@hanoi</td>
+                        <td><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a></td>
+
+                    </tr>
+
+                    <tr class="row100">
+                        <td class="column100 column1" data-column="column1">5</td>
+                        <td class="column100 column2" data-column="column2">Nguyễn Văn E</td>
+                        <td class="column100 column3" data-column="column3">01234567890</td>
+                        <td class="column100 column4" data-column="column4">email.test@mail.com</td>
+                        <td class="column100 column5" data-column="column5">hanoi@hanoi@hanoi</td>
+                        <td><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a></td>
+
+                    </tr>
+
+                    <tr class="row100">
+                        <td class="column100 column1" data-column="column1">6</td>
+                        <td class="column100 column2" data-column="column2">Nguyễn Văn B</td>
+                        <td class="column100 column3" data-column="column3">01234567890</td>
+                        <td class="column100 column4" data-column="column4">email.test@mail.com</td>
+                        <td class="column100 column5" data-column="column5">hanoi@hanoi@hanoi</td>
+                        <td><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a></td>
+
+                    </tr>
+
+                    <tr class="row100">
+                        <td class="column100 column1" data-column="column1">7</td>
+                        <td class="column100 column2" data-column="column2">Nguyễn Văn F</td>
+                        <td class="column100 column3" data-column="column3">01234567890</td>
+                        <td class="column100 column4" data-column="column4">email.test@mail.com</td>
+                        <td class="column100 column5" data-column="column5">hanoi@hanoi@hanoi</td>
+                        <td><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a></td>
+
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<!--===============================================================================================-->
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+<script src="js/main.js"></script>
+
+</body>
+</html>
